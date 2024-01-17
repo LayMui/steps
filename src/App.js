9ba0e48150ca67622cf40494bdf12efd7e956d16
LayMui <mui_ume@hotmail.com>
2024-01-17 13:54:43 +0800

    Use state hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 
 import './App.css';
+import { useState } from 'react';
+
+const messages = [
+  "Learn React *",
+  "Apply for jobs *",
+  "Invest your new income 🤑"
+
+]
 
 export default function App() {
+  const [step, setStep] = useState(1)
+  const [isOpen, setisOpen] = useState(true)
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep(step -1)
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1)
+    }
+  }
+
+
   return (
-   <div className="steps">Hello React</div>
+    <div>
+      <button className="close" onClick={()=>setisOpen(!isOpen)}>&times;</button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">Hello
+            Step {step}: {messages[step - 1 ]}
+          </p>
+
+          <div className="buttons">
+          <button 
+            style={{backgroundColor: '#7950f2' , color: '#ffff'}} 
+            onClick={handlePrevious} 
+          >Previous</button>
+          <button style={{backgroundColor: '#7950f2' , color: '#ffff'}} 
+          onClick={handleNext}>Next</button>
+          </div>
+        </div>
+    )}
+    </div>
   )
 }
 
